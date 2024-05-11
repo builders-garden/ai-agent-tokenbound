4fab434df41808db3cb089050eab3daf86a4e450
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2024-05-11 11:58:31 +0200

    wip

--- lib/account.ts
+++ lib/account.ts
@@ -1,12 +1,56 @@
+import { TokenboundClient } from '@tokenbound/sdk'
+import { base } from 'viem/chains'
 
 // get tokenbound account
 export async function getAccount({
+  tokenboundClient,
   tokenId,
 }: {
+  tokenboundClient: TokenboundClient
   tokenId: string
-}): Promise<{ account: string; txHash: string }> {
+}): Promise<{ account: string }> {
 
-  const collection = process.env.NFT_COLLECTION_ADDRESS
+    const collection = process.env.NFT_COLLECTION_ADDRESS
+    const account = tokenboundClient.getAccount({
+        tokenContract: collection as `0x${string}`,
+        tokenId,
+    })
   
-  return { account, txHash: account.txHash }
+    return { account}
 }
+
+// get NFT from account
+export async function getNFT({
+  tokenboundClient,
+  account,
+}: {
+  tokenboundClient: TokenboundClient
+  account: string
+}): Promise<{ tokenContract: string, tokenId: string, chainId: number }> {
+    const nft = await tokenboundClient.getNFT({
+        accountAddress: account as `0x${string}`,
+    })
+    const { tokenContract, tokenId, chainId } = nft
+
+    return { tokenContract, tokenId, chainId }
+}
+
+// Create a new account
+export async function createAccount({
+  tokenboundClient,
+  tokenId,
+}: {
+    tokenboundClient: TokenboundClient,
+    tokenId: string
+}): Promise<{ account: string }> {
+    const params = {
+        tokenContract: process.env.NFT_COLLECTION_ADDRESS as `0x${string}`,
+        tokenId: tokenId!,
+        chainId: base.id,
+    }
+
+    const { account, txHash } = await tokenboundClient.createAccount(params)
+
+    return { account}
+}
+
